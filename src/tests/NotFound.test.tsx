948f81a220725ef10efb3cf72ac2581d1d0d732c
henriqueAvner <avnerhdpb@gmail.com>
2023-09-12 09:59:12 -0300

    Feat: Requisito 4 - Testando o componente NotFound

--- src/tests/NotFound.test.tsx
+++ src/tests/NotFound.test.tsx
@@ -1,1 +1,13 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import { NotFound } from '../pages';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Testando componente notFound', () => {
+  it('Teste se a página contém um heading h2 com o texto Page requested not found', () => {
+    renderWithRouter(<NotFound />);
+    const heandingNotFound = screen.getByRole('heading', { name: /Page requested not found/i });
+    const imgNotFound = screen.getByAltText("Clefairy pushing buttons randomly with text I have no idea what i'm doing");
+    expect(heandingNotFound).toBeInTheDocument();
+    expect(imgNotFound).toBeInTheDocument();
+  });
+});
